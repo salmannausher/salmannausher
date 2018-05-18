ea0a4225eb54725dc343aeeb4a3d808cd37afb1c
Salman Nausher <salman.nausher@qbatch.com>
2018-05-18 17:55:26 +0500

    projects added dynamically, Active Admin Integrated, code moved into partials

---
+++ app/admin/projects.rb
@@ -0,0 +1,44 @@
+ActiveAdmin.register Project do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+permit_params do
+  permitted = [:title, :image]
+  permitted
+end
+
+index do
+    selectable_column
+    id_column
+    column :title
+    column :image
+    actions
+  end
+  form :html => { :enctype => "multipart/form-data" }  do |f|
+    f.inputs "Project" do
+      f.input :title, required: true
+      f.input :image, required: true, as: :file, :hint => (f.template.image_tag(f.object.image.url(:medium)) if f.object.image )
+    end
+    f.actions
+  end
+
+  show do |project|
+    attributes_table do
+      row :title
+      row "image" do
+        ul do
+          image_tag(project.image.url(:medium))
+        end
+      end
+    end
+  end
+end
